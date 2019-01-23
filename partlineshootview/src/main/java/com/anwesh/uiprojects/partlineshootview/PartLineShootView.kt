16dc32984228281c3188eeb7ee40bdeac08a2c16
Anwesh43 <anweshthecool0@gmail.com>
2019-01-23 08:13:39 +0530

    importing libraries and defining constants

--- partlineshootview/src/main/java/com/anwesh/uiprojects/partlineshootview/PartLineShootView.kt
+++ partlineshootview/src/main/java/com/anwesh/uiprojects/partlineshootview/PartLineShootView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.partlineshootview
 /**
  * Created by anweshmishra on 23/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.graphics.Paint.Cap
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.7f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
