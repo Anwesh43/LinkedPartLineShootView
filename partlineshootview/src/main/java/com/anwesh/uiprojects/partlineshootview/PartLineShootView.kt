cb213bfc7d95732d9d82404846309586f6d3aba1
Anwesh43 <anweshthecool0@gmail.com>
2019-01-23 08:41:05 +0530

    adding PartLineShooter class

--- partlineshootview/src/main/java/com/anwesh/uiprojects/partlineshootview/PartLineShootView.kt
+++ partlineshootview/src/main/java/com/anwesh/uiprojects/partlineshootview/PartLineShootView.kt
@@ -167,4 +167,27 @@ class PartLineShootView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class PartLineShooter(var i : Int) {
+
+        private var curr : PLSNode = PLSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
