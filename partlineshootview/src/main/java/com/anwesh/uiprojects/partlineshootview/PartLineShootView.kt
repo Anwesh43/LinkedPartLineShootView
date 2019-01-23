5d95a198f9c92833ddad9f6ebf1a402a634feb1e
Anwesh43 <anweshthecool0@gmail.com>
2019-01-23 08:38:53 +0530

    adding PLSNode class

--- partlineshootview/src/main/java/com/anwesh/uiprojects/partlineshootview/PartLineShootView.kt
+++ partlineshootview/src/main/java/com/anwesh/uiprojects/partlineshootview/PartLineShootView.kt
@@ -123,4 +123,48 @@ class PartLineShootView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PLSNode(var i : Int, val state : State = State()) {
+
+        private var next : PLSNode? = null
+        private var prev : PLSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = PLSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPLSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PLSNode {
+            var curr : PLSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
